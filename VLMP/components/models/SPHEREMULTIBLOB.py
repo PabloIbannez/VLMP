04453929b2f5f5a22d4ef65dbee01960b3213708
PabloPalaciosAlonso <pablo.pal@hotmail.com>
2024-05-31 10:01:37 +0200

    Now the number of tries to create the distribution is a parameter

--- VLMP/components/models/SPHEREMULTIBLOB.py
+++ VLMP/components/models/SPHEREMULTIBLOB.py
@@ -127,7 +127,8 @@ class SPHEREMULTIBLOB(modelBase):
                                                 "Ktethers",
                                                 "heightTethersThreshold",
                                                 "tethersPerBlob",
-                                                "thetaTethers"},
+                                                "thetaTethers",
+                                                "maxTries"},
                          requiredParameters  = {"K"},
                          definedSelections   = {"particleId"},
                          **params)
